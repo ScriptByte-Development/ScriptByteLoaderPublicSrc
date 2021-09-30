0a52fee4b403b29a89186af4a7fe5ce11fd47efb
Woke <66000596+woke999@users.noreply.github.com>
2021-09-30 14:27:41 -0400

    Update Data.cs to include cheat stuff

--- src/Handler/Data.cs
+++ src/Handler/Data.cs
@@ -14,8 +14,20 @@ namespace ScriptByteLoader.Handler
         public static string cheatrename = "MY_DLL_NAME.dll";
         public static string ConfigFile = @".\settings.config";
         public static string Discord = "https://discord.gg/DFcy5h9nuH";
+        public static string AdInvite;
+        public static string adtext;
         public static bool Injecting;
         public static bool Breached;
+        public static bool nxt;
+        /* These next bools will be how the Injector class decides which cheat to load
+         * It will also decide which one to rename as well
+        */
+        public static bool otc; //one tap crack
+        public static bool corsair; //corsair legit cheat
+        public static string cheatfolder =  @"C:\Program Files\ScriptByteLoader";
+        public static string otcfile = @"C:\Program Files\ScriptByteLoader\OTC.dll";
+        public static string corsairfile = @"C:\Program Files\ScriptByteLoader\corsair.dll";
+        public static string injectorfile = @"C:\Program Files\ScriptByteLoader\ScriptByteInjector.exe";
 
         public static bool IsAdministrator
         {
