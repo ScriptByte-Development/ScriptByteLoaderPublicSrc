197edb71c2c4821b19e8191a4ffc77c0ac6e9e3b
Woke <66000596+woke999@users.noreply.github.com>
2021-10-01 20:10:12 -0400

    Add files via upload

---
+++ src/Handler/Win32.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScriptByteLoader.Handler
+{
+    public static class Win32
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        //allocate console
+        public static extern bool AllocConsole();
+
+        //startup console message, only needs to be called once even if you load a new form, its basically a parent "form" now.
+        public static void Welcome()
+        {
+            Console.Title = $"{Application.ProductName} Debug Console - {Application.ProductVersion}";
+            Console.WriteLine($"Welcome to {Application.ProductName}");
+        }
+
+        //change the title when logged in 
+        public static void Login()
+        {
+            Console.Title = $"{Application.ProductName} Debug Console - {Application.ProductVersion} user - {Auth.User.Username} expires - {Auth.User.Expiry}";
+        }
+
+        //self explanatory 
+        public static void Error()
+        {
+            Console.WriteLine(Data.error);
+        }
+    }
+}
