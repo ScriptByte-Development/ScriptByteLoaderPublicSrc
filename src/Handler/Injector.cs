c21c365ea2b38a21611043ae871d39cf087bf4a1
Woke <66000596+woke999@users.noreply.github.com>
2021-10-04 21:12:12 -0400

    Cleanup class

--- src/Handler/Injector.cs
+++ src/Handler/Injector.cs
@@ -1,10 +1,11 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,8 +17,8 @@ namespace ScriptByteLoader.Handler
         {
             try
             {
-                CheckFolder();
-                CheckCheat();
+                fstream.CheckFolder();
+                fstream.CheckCheat();
             }
             catch (Exception ex)
             {
@@ -25,83 +26,10 @@ namespace ScriptByteLoader.Handler
             }
         }
 
-        public static void Rename(string currentname, string newname)
+        public static void LoadThread()
         {
-            try
-            {
-                System.IO.File.Move($"{currentname}", $"{newname}");
-                //MY_DLL_NAME.dll
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, Application.ProductName);
-            }
-        }
-
-        public static void CheckCheat()
-        {
-            try
-            {
-                if (!File.Exists(Data.injectorfile))
-                {
-                    using (var client = new WebClient())
-                    {
-                        client.DownloadFile("coolwebsite.com/corsair.dll/ScriptByteInjector.exe", Data.injectorfile);
-                    }
-                }
-                //doing if then a bool name c# automatically assumes its true, which in this case we want
-                if (Data.otc)
-                {
-                    if (File.Exists(Data.otcfile))
-                    {
-                        Rename("OTC.dll", "MY_CHEAT_NAME.dll");
-                        Process.Start("ScriptByte Injector.exe");
-                        Rename("MY_CHEAT_NAME.dll", "OTC.dll");
-                    }
-                    else
-                    {
-                        using (var client = new WebClient())
-                        {
-                            client.DownloadFile("coolwebsite.com/OTC.dll", Data.otcfile);
-                        }
-                    }
-                }
-                if (File.Exists(Data.corsairfile))
-                {
-                    Rename("corsair.dll", "MY_CHEAT_NAME.dll");
-                    Process.Start("ScriptByte Injector.exe");
-                    Rename("MY_CHEAT_NAME.dll", "corsair.dll");
-                }
-                else
-                {
-                    using (var client = new WebClient())
-                    {
-                        client.DownloadFile("coolwebsite.com/corsair.dll", Data.otcfile);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, Application.ProductName);
-            }
-        }
-
-        public static void CheckFolder()
-        {
-            try
-            {
-                string Path = @"C:\Program Files\ScriptByteLoader";
-                bool exists = System.IO.Directory.Exists(Path);
-                if (!exists)
-                {
-                    System.IO.Directory.CreateDirectory(Path);
-                    MessageBox.Show($"ScriptByteLoader directory not found, making directory for {Application.ProductName}", Application.ProductName);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            Thread thread = new Thread(Load);
+            thread.Start();
         }
     }
 }
